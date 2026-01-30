1db46b6365525744e147bc675652f5abf8dec054
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2026-01-30 02:08:37 +0100

    Stream apply progress events to UI in real-time

--- ui/electron/preload.ts
+++ ui/electron/preload.ts
@@ -3,7 +3,6 @@ import { contextBridge, ipcRenderer } from 'electron'
 // Expose IPC methods to the renderer process
 contextBridge.exposeInMainWorld('electron', {
   invoke: (channel: string, ...args: unknown[]) => {
-    // Whitelist of allowed channels
     const validChannels = [
       'get_systems',
       'get_config',
@@ -14,6 +13,9 @@ contextBridge.exposeInMainWorld('electron', {
       'get_install_status',
       'install_app',
       'uninstall_app',
+      'sync_status',
+      'sync_add_device',
+      'sync_remove_device',
     ]
 
     if (validChannels.includes(channel)) {
@@ -21,5 +23,19 @@ contextBridge.exposeInMainWorld('electron', {
     }
 
     throw new Error(`Invalid IPC channel: ${channel}`)
+  },
+
+  on: (channel: string, callback: (...args: unknown[]) => void) => {
+    const validChannels = ['apply:progress']
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (_event, ...args) => callback(...args))
+    }
+  },
+
+  off: (channel: string, callback: (...args: unknown[]) => void) => {
+    const validChannels = ['apply:progress']
+    if (validChannels.includes(channel)) {
+      ipcRenderer.removeListener(channel, callback)
+    }
   },
 })
